aef92981804f730861a70646cc92a22bc33ef7a0
Utkarsh Krishna <utkarshmark14@gmail.com>
2026-02-07 14:45:31 +0530

    clerk routing bug resolved

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
+const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up", "/", "/home"]);
 
 const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
 
@@ -19,13 +19,13 @@ export default clerkMiddleware((auth, req) => {
     // if not logged-in
     if (!userId) {
         // if user is not logged-in and trying to access a protected route
-        if (!isPublicApiRoute(req) && !isPublicApiRoute(req)) {
-            return NextResponse.redirect(new URL("/signin", req.url));
+        if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
 
         // if the request is for a protected API and the user is not logged-in
         if (isApiRequest && !isPublicApiRoute(req)) {
-            return NextResponse.redirect(new URL("/signin", req.url));
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
     }
 
