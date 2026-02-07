cc10342e53276a9838cfb6bc165769391fe9a5b1
Utkarsh Krishna <utkarshmark14@gmail.com>
2026-02-07 14:10:53 +0530

    clerk auth logic done

---
+++ proxy.ts
@@ -0,0 +1,37 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
+
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware((auth, req) => {
+    const { userId } = auth();
+    const currentUrl = new URL(req.url);
+    const isAccessingDashboard = currentUrl.pathname === "/home";
+    const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+    // if user is logged-in and accessing a public route but not the dashboard
+    if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL("/home", req.url));
+    }
+
+    // if not logged-in
+    if (!userId) {
+        // if user is not logged-in and trying to access a protected route
+        if (!isPublicApiRoute(req) && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/signin", req.url));
+        }
+
+        // if the request is for a protected API and the user is not logged-in
+        if (isApiRequest && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/signin", req.url));
+        }
+    }
+
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
